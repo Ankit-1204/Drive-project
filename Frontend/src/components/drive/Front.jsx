7e6a856576c152dc74686a34912fb20e76956836
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-07-07 01:06:12 +0530

    file and folder button changed

--- Frontend/src/components/drive/Front.jsx
+++ Frontend/src/components/drive/Front.jsx
@@ -4,7 +4,10 @@ import FolderModal from "./FolderModal.jsx";
 import FileModal from "./FileModal.jsx";
 import { useFolder } from "../Hooks/useFolder.jsx";
 import { Link, useParams } from "react-router-dom";
+import { FaFile } from "react-icons/fa";
+import { IoFolderOpen } from "react-icons/io5";
 import FolderPath from "./FolderPath.jsx";
+import { FolderIcon } from "./Icon.jsx";
 
 const Front=()=>{
     let {Id}=useParams();
@@ -39,10 +42,10 @@ const Front=()=>{
             </div>
         </div>
     </div>
-    
-        {folder && childFolders && <div className="flex w-full flex-col md:flex-row mx-auto px-9 py-4">
-            {childFolders.map((folder)=>(<Link className=" bg-gray-600 p-3 m-2 rounded-md md:m-8" key={folder.key} to={'/folder/'+(folder.key)} >{folder.name}</Link>))}
-            {childFiles.map((file)=>(<Link className=" bg-gray-600 p-3 m-2 rounded-md md:m-8" key={file.key} >{file.name}</Link>))}
+    {/* className=" bg-gray-600 p-3 m-2 rounded-md md:m-8" */}
+        {folder && childFolders && <div className="grid grid-cols-1 md:grid-cols-4 py-5 px-5 gap-6">
+            {childFolders.map((folder)=>(<div><Link key={folder.key} to={'/folder/'+(folder.key)} ><button className=" px-6 py-3.5 text-base font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><IoFolderOpen className=" mr-3 w-5 h-5 md:w-9 md:h-9" /> {folder.name}</button></Link></div>))}
+            {childFiles.map((file)=>(<div><Link key={file.key}><button className=" px-6 py-3.5 text-base font-medium text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><FaFile className=" mr-3 w-5 h-5 md:w-9 md:h-9" /> {file.name}</button></Link></div>))}
         </div>}
     
     </div>
--- Frontend/src/components/drive/Icon.jsx
+++ Frontend/src/components/drive/Icon.jsx
@@ -1,1 +1,13 @@
-import React from "react";
+import React, { Children } from "react";
+import { IoFolderOpen } from "react-icons/io5";
+import { FaFile } from "react-icons/fa";
+
+export const FolderIcon=()=>{
+
+    return(
+        <div className="">
+            <FaFile />
+        </div>
+    )
+
+}
